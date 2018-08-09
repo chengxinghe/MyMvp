923cebed0fa419f55f44fd4d89aa8c3a1130b111
Mr <1303020489@qq.com>
2018-08-09 09:40:40 +0800

    增加注释

--- mychip/src/main/java/com/cxh/mychip/activity/MainActivity.java
+++ mychip/src/main/java/com/cxh/mychip/activity/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //弹出dialog
         mShapeCornerView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
